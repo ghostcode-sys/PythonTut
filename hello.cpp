78733cf4e52ba9f887aa825b162dbba229bfd9b5
ghost-sys <prashantchandel2103@gmail.com>
2022-12-30 00:07:30 +0530

    first tutorail

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int main()
+ {
+ /* CODE HERE */ 
+  cout<<"Hello world"<<endl;for(int i=0; i<5; i++){  cout<<i<<endl;}return 0;
+}
---
+++ hello.py
@@ -0,0 +1,7 @@
+
+
+# this is to print variable
+""" hello my name is Prashant
+    hello my 
+"""
+print(type123)
